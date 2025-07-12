139be93f551ad26d261ef90bec56f039f67da6c6
Andrea Latorre <latorre.andrea.93@gmail.com>
2025-07-12 18:13:21 +0200

    fix 03 auto sending email

--- aura_visual/config.py
+++ aura_visual/config.py
@@ -17,7 +17,8 @@ class Config:
     # Flask-Mail Configuration
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.zoho.eu')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True').lower() == 'true'
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'default_username')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'default_password')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'info@auravisual.dk')
